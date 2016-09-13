1382dbce87a2fc7b91e24c5c0575628017ed0bcb
bymind <studio@bymind.ru>
2016-09-13 04:45:08 +0300

    Deleted spaces.

--- dev/app/miner.php
+++ dev/app/miner.php
@@ -6,8 +6,5 @@ require_once 'core/config.php'; //config file
 require_once 'core/model.php'; // models parent class
 require_once 'core/view.php'; // views parent class
 require_once 'core/controller.php'; // controllers parent class
-
-
 require_once 'core/route.php'; // routing
-
 Route::start(); // starting routing
