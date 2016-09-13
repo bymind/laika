f14542a116069630f8b9341b90c1443ec4ff2ce4
bymind <studio@bymind.ru>
2016-09-13 04:35:59 +0300

    moved index

---
+++ dev/index.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+	<meta charset="UTF-8">
+	<title>Travelguitar from Russia - Laika.pro</title>
+</head>
+<body>
+	<header>
+	
+		<nav>
+			<ul>
+				<li><a href="">1 пункт</a></li>
+				<li><a href="">2 пункт</a></li>
+				<li><a href="">3 пункт</a></li>
+				<li><a href="">4 пункт</a></li>
+				<li><a href="">5 пункт</a></li>
+				<li><a href="">6 пункт</a></li>
+				<li><a href="">7 пункт</a></li>
+				<li><a href="">8 пункт</a></li>
+				<li><a href="">9 пункт</a></li>
+				<li><a href="">10 пункт</a></li>
+			</ul>
+		</nav>
+	
+	</header>
+	
+	<section class="section-1">
+	
+	
+	</section>
+	
+	<section class="section-2">
+	
+	
+	</section>
+	
+	<section class="section-3">
+	
+	
+	</section>
+	
+	<section class="section-4">
+	
+	
+	</section>
+	
+	<section class="section-5">
+	
+	
+	</section>
+	
+	<footer>
+	
+	</footer>
+	
+</body>
+</html>
