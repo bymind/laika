a3b0a05fc01900fffab54db95a75b30a77707d0c
bymind <studio@bymind.ru>
2016-09-13 04:43:23 +0300

    Перенаправление с публичного index.php на точку входа. Сборка точки входа в miner.php.

---
+++ dev/app/miner.php
@@ -0,0 +1,13 @@
+<?php
+
+// include core files (core classes)
+
+require_once 'core/config.php'; //config file
+require_once 'core/model.php'; // models parent class
+require_once 'core/view.php'; // views parent class
+require_once 'core/controller.php'; // controllers parent class
+
+
+require_once 'core/route.php'; // routing
+
+Route::start(); // starting routing
