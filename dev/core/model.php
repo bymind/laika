cedfd4b1b4f106eac8105375f57b126ac30c414a
bymind <studio@bymind.ru>
2016-09-14 03:14:47 +0300

    added main classes model and view

---
+++ dev/core/model.php
@@ -0,0 +1,18 @@
+<?php
+
+class Model
+{
+	public function get_data()
+	{
+		
+	}
+
+	public function get_login($login)
+	{
+		$select = mysql_query("SELECT * FROM users WHERE login = '$login'")or die(mysql_error());
+		
+		$ds = mysql_fetch_assoc($select);
+
+		return $ds;
+	}
+}
