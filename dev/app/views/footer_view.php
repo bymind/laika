0d1819f27c57c7e8dbbb0eb627f00fcbe00c67f4
bymind <studio@bymind.ru>
2016-10-14 17:24:48 +0300

    footer view added

---
+++ dev/app/views/footer_view.php
@@ -0,0 +1,17 @@
+<footer>
+	<nav class="main_menu footer_menu">
+		<ul class="menu_text_units">
+			<li><a href="">About</a></li>
+			<li><a href="">News</a></li>
+			<li><a href="">Shipping and payment</a></li>
+			<li><a href="">Contacts</a></li>
+		</ul>
+		
+		<ul class="menu_ui_units">
+			<li><a href="" class="active">En</a></li>
+			<li><a href="">Rus</a></li>
+			<li><a href="" class="active">Fb</a></li>
+			<li><a href="" class="active">Inst</a></li>
+		</ul>
+	</nav>
+</footer>
