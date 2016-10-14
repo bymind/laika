3173d7262d6b13d44d55dcf4e63d777df7520d84
bymind <studio@bymind.ru>
2016-10-14 04:15:55 +0300

    main page controller editing

--- dev/app/controllers/controller_main.php
+++ dev/app/controllers/controller_main.php
@@ -12,7 +12,20 @@ class Controller_Main extends Controller
 	public function action_index()
 	{
 		$pageData = $this->model->getData('main_page');
-		echo $pageData['text'],"<br>";
+		$this->view->generate(
+			'main_view.php',
+			'template_view.php',
+			array(
+					'title'=> $pageData['title'],
+					'style'=>'public/template.css',
+					'style_content'=>'public/main.css',
+					'active_menu' => 'menu-item-1',
+					'pageId' => 'main',
+					'pageData' => $pageData
+				),
+			'navigation_view.php',
+			'footer_view.php'
+			);
 		return 0;
 	}
 }
