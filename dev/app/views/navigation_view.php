f4401e8825ea1165e2e637fd8362f2f1af230a2e
bymind <studio@bymind.ru>
2016-10-14 04:15:29 +0300

    Added navigation vew

---
+++ dev/app/views/navigation_view.php
@@ -0,0 +1,15 @@
+<nav class="main_menu">
+	<ul class="menu_text_units">
+		<li><a href="">About</a></li>
+		<li><a href="">News</a></li>
+		<li><a href="">Shipping and payment</a></li>
+		<li><a href="">Contacts</a></li>
+	</ul>
+	
+	<ul class="manu_ui_units">
+		<li><a href="">En</a></li>
+		<li><a href="">Rus</a></li>
+		<li><a href="">Fb</a></li>
+		<li><a href="">Inst</a></li>
+	</ul>
+</nav>
