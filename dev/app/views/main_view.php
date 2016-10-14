a913ea228ab2791a0df14bbc10178bfe0a89d4aa
bymind <studio@bymind.ru>
2016-10-14 17:24:22 +0300

    added <header> block

--- dev/app/views/main_view.php
+++ dev/app/views/main_view.php
@@ -1,3 +1,22 @@
+<header>
+	<div class="hue"></div>
+	<div class="header_content">
+		<div class="logo_big">
+		</div>
+		<div class="slogan">
+			<h1>Crafted travel guitars</h1>
+		</div>
+		<div class="header_preorder">
+			<button class="ghost">
+				Preorder guitar
+			</button>
+			<div class="btn_undertext">
+				Worldwide shipping
+			</div>
+		</div>
+	</div>
+</header>
+
 <div class="container main_page__container">
 
 	<?php
