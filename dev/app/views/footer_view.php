de8f0e9b80fd35f943a7d23d2e7f69c1f3e916f3
bymind <studio@bymind.ru>
2016-10-16 04:00:03 +0300

    Footer menu edit

--- dev/app/views/footer_view.php
+++ dev/app/views/footer_view.php
@@ -1,17 +1,15 @@
 <footer>
 	<nav class="main_menu footer_menu">
 		<ul class="menu_text_units">
-			<li><a href="">About</a></li>
-			<li><a href="">News</a></li>
-			<li><a href="">Shipping and payment</a></li>
-			<li><a href="">Contacts</a></li>
+			<li><a href="/#">About</a></li>
+			<li><a href="/#">News</a></li>
+			<li><a href="/#">Shipping and payment</a></li>
+			<li><a href="/#">Contacts</a></li>
 		</ul>
 		
 		<ul class="menu_ui_units">
-			<li><a href="" class="active">En</a></li>
-			<li><a href="">Rus</a></li>
-			<li><a href="" class="active">Fb</a></li>
-			<li><a href="" class="active">Inst</a></li>
+			<li><a title="Facebook" href="https://www.fb.com/LaikaProGuitar" target="_blank" class="facebook active"></a></li>
+			<li><a title="Instagram" href="https://www.instagram.com/russiantravelguitar/" target="_blank" class="instagram active"></a></li>
 		</ul>
 	</nav>
 </footer>
