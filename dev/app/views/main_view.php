fd7ed0a3f5a52ab7904ebf347a709a67c1523a3a
bymind <studio@bymind.ru>
2016-10-18 11:50:54 +0300

    Preorder block

--- dev/app/views/main_view.php
+++ dev/app/views/main_view.php
@@ -22,7 +22,7 @@
 </div>
 
 
-<div class="container main_page__container">
+<div class="container">
 
 	<?php
 		// echo $pageDataView['text'],'<br>';
@@ -30,14 +30,70 @@
 
 	<div class="row mb-100">
 			<div class="col-xs-6 col-xs-offset-1">
-				<div class="awesome-block">1</div>
+				<div class="awesome-block">
+					<div class="icon rubanok">
+					</div>
+					<span class="awesome-title mb-20">
+						Crafted
+					</span>
+					<div class="awesome-text">
+						Сделано пиздатыми мозолистыми русскими руками, используя только топор, напильник, станок с ЧПУ и пару бутылок пива.
+					</div>
+				</div>
 			</div>
 			<div class="col-xs-6">
-				<div class="awesome-block">2</div>
+				<div class="awesome-block">
+					<div class="icon sound">
+					</div>
+					<span class="awesome-title mb-20">
+						Awesome sound
+					</span>
+					<div class="awesome-text">
+						Звучит как твоя мамка в свои лучшие годы. Хотя не - лучше.
+					</div>
+				</div>
 			</div>
 			<div class="col-xs-6">
-				<div class="awesome-block">3</div>
+				<div class="awesome-block">
+					<div class="icon stars">
+					</div>
+					<span class="awesome-title mb-20">
+						Exclusive
+					</span>
+					<div class="awesome-text">
+						Хуй найдешь еще одну такую. Каждая гитара создана под определенным градусом алкогольного опьянения, так что каждая - уникальна.
+					</div>
+				</div>
 			</div>
 	</div>
 
 </div>
+
+<div class="container-fluid grass-bg guitar-preorder pb-50 pt-50">
+
+	<div class="row">
+		<div class="container">
+			<div class="col-xs-10">
+				<div class="preorder-block">
+				</div>
+			</div>
+			<div class="col-xs-10">
+				<div class="preorder-block">
+					<span class="preorder-title mb-50">
+						Preorder
+					</span>
+					<span class="preorder-subtitle">
+						New model Travel Guitar
+					</span>
+					<div class="preorder-text">
+						Our new model line is on the way.
+						You can preorder this guitar and get it when we finish it.
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+
+
+</div>
+
