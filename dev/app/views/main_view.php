6850d0d60c5e84ac529adc60f6cd8a75223873db
bymind <studio@bymind.ru>
2016-10-18 07:57:21 +0300

    Edited with Bootstrap

--- dev/app/views/main_view.php
+++ dev/app/views/main_view.php
@@ -1,26 +1,45 @@
-<header>
-	<div class="hue"></div>
-	<div class="header_content">
-		<div class="logo_big">
-		</div>
-		<div class="slogan">
-			<h1>Crafted travel guitars</h1>
-		</div>
-		<div class="header_preorder">
-			<button class="ghost">
-				Preorder guitar
-			</button>
-			<div class="btn_undertext">
-				Worldwide shipping
+<div class="container-fluid">
+	<div class="row">
+		<header>
+			<div class="hue"></div>
+			<div class="header_content">
+				<div class="logo_big">
+				</div>
+				<div class="slogan">
+					<h1>Crafted travel guitars</h1>
+				</div>
+				<div class="header_preorder">
+					<button class="ghost">
+						Preorder guitar
+					</button>
+					<div class="btn_undertext">
+						Worldwide shipping
+					</div>
+				</div>
 			</div>
-		</div>
+		</header>
 	</div>
-</header>
+</div>
+
 
 <div class="container main_page__container">
 
 	<?php
-		echo $pageDataView['text'],'<br>';
+		// echo $pageDataView['text'],'<br>';
 	?>
+
+	<div class="row">
+		<div class="col-xs-18 col-xs-offset-1">
+			<div class="col-xs-6">
+				<div class="awesome-block">1</div>
+			</div>
+			<div class="col-xs-6">
+				<div class="awesome-block">2</div>
+			</div>
+			<div class="col-xs-6">
+				<div class="awesome-block">3</div>
+			</div>
+		</div>
+	</div>
 
 </div>
