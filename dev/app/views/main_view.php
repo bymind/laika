bbdf26c0aa470470a738b73dd55a12247a4c0c38
bymind <studio@bymind.ru>
2016-10-18 20:45:59 +0300

    Main page edit

--- dev/app/views/main_view.php
+++ dev/app/views/main_view.php
@@ -69,7 +69,7 @@
 
 </div>
 
-<div class="container-fluid grass-bg guitar-preorder pb-50 pt-50">
+<div class="container-fluid grass-bg guitar-preorder pb-50 pt-50 mb-100">
 
 	<div class="row">
 		<div class="container">
@@ -85,15 +85,237 @@
 					<span class="preorder-subtitle">
 						New model Travel Guitar
 					</span>
-					<div class="preorder-text">
-						Our new model line is on the way.
+					<div class="preorder-text mb-50">
+						Our new model line is on the way.<br>
 						You can preorder this guitar and get it when we finish it.
+					</div>
+					<div class="preorder-price">
+						$ 180
+					</div>
+					<div class="preorder-btn">
+						<button class="ghost">
+							Preorder now!
+						</button>
+						<div class="btn_undertext">
+							Worldwide shipping
+						</div>
 					</div>
 				</div>
 			</div>
 		</div>
 	</div>
 
+</div>
+
+<div class="container shop-bg mb-100">
+
+	<div class="row">
+		<div class="col-xs-20 mb-100">
+			<span class="shop-title">
+				Shop
+			</span>
+		</div>
+	</div>
+
+	<div class="row mb-50">
+
+		<div class="col-xs-5">
+			<div class="guitar-360">
+			</div>
+		</div>
+
+		<div class="col-xs-5 guitar-media">
+			<div class="media-row">
+				<div class="col-xs-10 pl-0">
+					<div class="media-item">
+						<img src="/img/guitar-sec1.jpg" alt="">
+					</div>
+				</div>
+				<div class="col-xs-10 pr-0">
+					<div class="media-item">
+						<img src="/img/guitar-sec2.jpg" alt="">
+					</div>
+				</div>
+			</div>
+			<div class="media-row">
+				<div class="col-xs-10 pl-0">
+					<div class="media-item">
+						<img src="/img/guitar-sec3.jpg" alt="">
+					</div>
+				</div>
+				<div class="col-xs-10 pr-0">
+					<div class="media-item">
+						<img src="/img/guitar-sec4.jpg" alt="">
+					</div>
+				</div>
+			</div>
+			<div class="media-row video">
+				<div class="col-xs-20 p-0">
+					<div class="media-item">
+						<iframe height="160" width="280" src="https://www.youtube.com/embed/PMrmKykJSog" frameborder="0" allowfullscreen></iframe>
+					</div>
+				</div>
+			</div>
+			<div class="media-row audio">
+				<div class="col-xs-20 p-0">
+					<div class="media-item">
+						<iframe width="100%" height="100" scrolling="no" frameborder="no" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/223663988&amp;auto_play=false"></iframe>
+					</div>
+				</div>
+			</div>
+		</div>
+
+		<div class="col-xs-10">
+			<span class="guitar-details-title">
+				Travel guitar "Laika-First"
+			</span>
+			<span class="guitar-price mb-50">
+				$ 200
+			</span>
+			<div class="guitar-text mb-50">
+				Our new model line is on the way.<br>
+				This is a first one of this line.<br>
+				<br>
+				Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+			</div>
+			<div class="col-xs-10 pl-0">
+				<button class="buy">
+					Buy now
+				</button>
+			</div>
+			<div class="col-xs-10">
+				<div class="paypal-img"></div>
+			</div>
+		</div>
+
+	</div>
+
+	<div class="row mb-50">
+
+		<div class="col-xs-5">
+			<div class="secondary-item sec-item1">
+				<div class="cover"></div>
+				<div class="item-info">
+					<span class="item-title">
+						Case for travel guitar
+					</span>
+					<div class="item-text">
+						Case for your new travel guitar.<br>
+						Perfect for new model Laika.
+					</div>
+					<div class="col-xs-10 pl-20">
+						<div class="item-price">$ 60</div>
+					</div>
+					<div class="col-xs-10">
+						<button class="secondary-item-btn">
+							Add to cart
+						</button>
+					</div>
+				</div>
+			</div>
+		</div>
+		
+		<div class="col-xs-5">
+			<div class="secondary-item  sec-item2">
+				<div class="cover"></div>
+				<div class="item-info">
+					<span class="item-title">
+						COOL T-SHIRT "LAIKA"
+					</span>
+					<div class="item-text">
+						Awesome T-shirt from master, with the smell of his sweat, obviously.
+					</div>
+					<div class="col-xs-10 pl-20">
+						<div class="item-price">$ 50</div>
+					</div>
+					<div class="col-xs-10">
+						<button class="secondary-item-btn">
+							Add to cart
+						</button>
+					</div>
+				</div>
+			</div>
+		</div>
+		
+		<div class="col-xs-5">
+			<div class="secondary-item sec-item3">
+				<div class="cover"></div>
+				<div class="item-info">
+					<span class="item-title">
+						GUITAR PICK
+					</span>
+					<div class="item-text">
+						Guitar pick with stupid fucking terrible logo
+					</div>
+					<div class="col-xs-10 pl-20">
+						<div class="item-price">$ 10</div>
+					</div>
+					<div class="col-xs-10">
+						<button class="secondary-item-btn">
+							Add to cart
+						</button>
+					</div>
+				</div>
+			</div>
+		</div>
+		
+		<div class="col-xs-5">
+			<div class="secondary-item sec-item4">
+				<div class="cover"></div>
+				<div class="item-info">
+					<span class="item-title">
+						THE SAME SHIT
+					</span>
+					<div class="item-text">
+						One more awesome and incredible piece of shit
+					</div>
+					<div class="col-xs-10 pl-20">
+						<div class="item-price">$ 180</div>
+					</div>
+					<div class="col-xs-10">
+						<button class="secondary-item-btn">
+							Add to cart
+						</button>
+					</div>
+				</div>
+			</div>
+		</div>
+
+	</div>
+
+	<div class="row">
+		<div class="container">
+			<button class="all">
+				Show all
+			</button>
+		</div>
+	</div>
 
 </div>
 
+<div class="container-fluid grass-bg pt-50">
+
+	<div class="row mb-50">
+		<div class="container">
+			<span class="video-title mb-50">
+				Videos
+			</span>
+			<div class="col-xs-6 col-xs-offset-1">
+				<div class="video-item">
+					<iframe height="100%" width="100%" src="https://www.youtube.com/embed/PMrmKykJSog" frameborder="0" allowfullscreen></iframe>
+				</div>
+			</div>
+			<div class="col-xs-6">
+				<div class="video-item">
+					<iframe width="100%" height="100%" src="https://www.youtube.com/embed/VtaIxGSWkYE" frameborder="0" allowfullscreen></iframe>
+				</div>
+			</div>
+			<div class="col-xs-6">
+				<div class="video-item">
+					<iframe width="100%" height="100%" src="https://www.youtube.com/embed/kXK2cdRoimQ" frameborder="0" allowfullscreen></iframe>
+				</div>
+			</div>
+		</div>
+	</div>
+
+</div>
