3490dcc0a0335480419605e513390159e89d6be3
bymind <studio@bymind.ru>
2016-10-24 07:21:10 +0300

    Display $scripts_content

--- dev/app/views/template_view.php
+++ dev/app/views/template_view.php
@@ -66,6 +66,7 @@
 	*/ ?>
 <script src="https://code.jquery.com/jquery-3.1.1.min.js" integrity="sha256-hVVnYaiADRTO2PzUGmuLJr8BLUSjGIZsDYGmIJLv2b8=" crossorigin="anonymous"></script>
 <script src="/js/bootstrap/bootstrap.min.js"></script>
+<script src="<?php echo $scripts_content; ?>"></script>
 
 </body>
 </html>
