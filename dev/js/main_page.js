975abfc063bb33f3c3752c707dbbaa861adb53de
bymind <studio@bymind.ru>
2016-10-24 08:12:56 +0300

    btn handlers

--- dev/js/main_page.js
+++ dev/js/main_page.js
@@ -5,6 +5,7 @@
 
 $(function() {
 	VClickInit();
+	BtnClickInit();
 });
 
 
@@ -30,8 +31,62 @@ function VClickInit() {
 function VClickHandler(obj) {
 	var vStrBegin = '<iframe height="100%" width="100%" src="https://www.youtube.com/embed/';
 	var vStrEnd = '?autoplay=1" frameborder="0" allowfullscreen></iframe>';
-	var vStr = vStrBegin + obj.data('videoId') + vStrEnd;
+	var vStr = vStrBegin + obj.data().videoId + vStrEnd;
 	var vContainer = obj.parent();
 	vContainer.append(vStr);
 	obj.remove();
+}
+
+
+/*
+* BtnClickInit()
+* Initialisation click to the button;
+* @return 0
+*/
+function BtnClickInit() {
+	$(document).on('click', 'button', function(event) {
+		BtnClickHandler($(this));
+	});
+	return 0;
+}
+
+
+/*
+* BtnClickHandler(obj)
+* Handler click to the button;
+* @param obj
+* @return 0
+*/
+function BtnClickHandler(obj) {
+	var target = obj.data().target;
+
+	switch(target) {
+
+		case 'preorder':
+			ModalPreorderGo(obj);
+			break;
+
+		default:
+			break;
+	}
+
+	return 0;
+}
+
+
+/*
+* ModalPreorderGo(obj)
+* Modal window for preorder
+* @param obj
+* @return 0
+*/
+function ModalPreorderGo(obj) {
+
+	var target = obj.data().target;
+	var mObj = $('#'+target);
+
+	mObj.modal();
+
+	console.log('Hello, modal!');
+
 }
