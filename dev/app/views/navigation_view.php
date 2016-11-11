6a00c849373e0f225f3796faaa3e9df4f7eb4dbd
bymind <studio@bymind.ru>
2016-11-11 12:27:14 +0300

    adaptive editing

--- dev/app/views/navigation_view.php
+++ dev/app/views/navigation_view.php
@@ -10,11 +10,24 @@
 				</ul>
 			</div>
 			<div class="col-xs-20 col-md-8">
+				<div class="visible-xs visible-sm menu_icon_div">
+					<button class="menu_icon" data-target="mobile-menu">
+						<span class="menu_draw"></span>
+					</button>
+				</div>
 				<ul class="menu_ui_units">
 					<li><a href="/eng" title="English" class="active">En</a></li>
 					<li><a href="/ru" title="Русский">Rus</a></li>
 					<li><a title="Facebook" href="https://www.fb.com/LaikaProGuitar" target="_blank" class="facebook active"></a></li>
 					<li><a title="Instagram" href="https://www.instagram.com/russiantravelguitar/" target="_blank" class="instagram active"></a></li>
+				</ul>
+			</div>
+			<div class="col-xs-20 visible-xs visible-sm mobile_div_ul disabled">
+				<ul class="menu_text_units mobile_ul">
+					<li><a href="/about">About</a></li>
+					<li><a href="/news">News</a></li>
+					<li><a href="/shipping-and-payment">Shipping and payment</a></li>
+					<li><a href="/contacts">Contacts</a></li>
 				</ul>
 			</div>
 		</div>
